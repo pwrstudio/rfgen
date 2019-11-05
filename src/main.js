96bd56cfa0422e295ad4419c8df7164bf44a1b8f
pwrstudio <cell@pwr.site>
2019-11-05 17:39:16 +0100

    Enable sentry

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import App from './App.svelte'
 
-// Sentry.init({
-//   dsn: 'https://6aa03223337a437ba745426629062602@sentry.io/1779548'
-// })
+Sentry.init({
+  dsn: 'https://6aa03223337a437ba745426629062602@sentry.io/1779548'
+})
 
 const app = new App({
   target: document.body
